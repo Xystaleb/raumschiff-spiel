4c4b2aec5d384ae8acaa7e333ea604b27c1e55fb
Jannik <jannik.brozy@student.fh-kiel.de>
2023-06-15 15:35:34 +0200

    projectiles is back

---
+++ src/models/projectile.js
@@ -0,0 +1,13 @@
+import GameObject from "./game-object.js";
+
+export default class Projectile extends GameObject {
+    constructor(x, y, width, height) {
+        super(x, y, width, height);
+    }
+
+    build() {
+        this.element = document.createElement('div');
+        this.element.className = 'projectile';
+        super.build();
+    }
+}
